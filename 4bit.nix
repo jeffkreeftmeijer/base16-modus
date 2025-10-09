d9cd162fcd874a6001b9b157711270fbbdaec658
Jeff Kreeftmeijer <jeff@kreeft.me>
2025-10-09 17:55:59 +0200

    Move 4bit colors to colors/4bit.nix

---
+++ 4bit.nix
@@ -0,0 +1,20 @@
+{
+  palette = {
+    base00 = "#000000"; # black
+    base01 = "#800000"; # red
+    base02 = "#008000"; # green
+    base03 = "#808000"; # yellow
+    base04 = "#000080"; # blue
+    base05 = "#800080"; # magenta
+    base06 = "#008080"; # cyan
+    base07 = "#c0c0c0"; # white
+    base08 = "#555555"; # bright black
+    base09 = "#ff0000"; # bright red
+    base0A = "#00ff00"; # bright green
+    base0B = "#ffff00"; # bright yellow
+    base0C = "#0000ff"; # bright blue
+    base0D = "#ff00ff"; # bright magenta
+    base0E = "#00ffff"; # bright cyan
+    base0F = "#ffffff"; # bright white
+  };
+}
