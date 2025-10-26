42be2848d020f941ba6426052e677f13bf803ef6
Jeff Kreeftmeijer <jeff@kreeft.me>
2025-10-26 16:02:52 +0100

    Swap ef-themes colors
    Swap the colors in ef-themes.el around to roughly match the ones in
    the dracula
    theme (https://github.com/dracula/base16-dracula-scheme/blob/master/dracula.yaml)
    as a sensible starting point.

--- ef-themes.el
+++ ef-themes.el
@@ -10,14 +10,14 @@
       "base05: \"" (substring (car (alist-get 'fg-main palette)) 1) "\" # fg-main\n"
       "base06: \"" (substring (car (alist-get 'fg-dim palette)) 1) "\" # fg-dim\n"
       "base07: \"" (substring (car (alist-get 'fg-alt palette)) 1) "\" # bg-alt\n"
-      "base08: \"" (substring (car (alist-get 'magenta-cooler palette)) 1) "\" # magenta-cooler\n"
-      "base09: \"" (substring (car (alist-get 'red-cooler palette)) 1) "\" # red-cooler\n"
-      "base0A: \"" (substring (car (alist-get 'magenta-warmer palette)) 1) "\" # magenta-warmer\n"
-      "base0B: \"" (substring (car (alist-get 'yellow palette)) 1) "\" # yellow\n"
-      "base0C: \"" (substring (car (alist-get 'yellow-warmer palette)) 1) "\" # yellow-wamer\n"
+      "base08: \"" (substring (car (alist-get 'red-cooler palette)) 1) "\" # red-cooler\n"
+      "base09: \"" (substring (car (alist-get 'magenta-cooler palette)) 1) "\" # magenta-cooler\n"
+      "base0A: \"" (substring (car (alist-get 'yellow palette)) 1) "\" # yellow\n"
+      "base0B: \"" (substring (car (alist-get 'yellow-warmer palette)) 1) "\" # yellow-warmer\n"
+      "base0C: \"" (substring (car (alist-get 'cyan-cooler palette)) 1) "\" # cyan-cooler\n"
       "base0D: \"" (substring (car (alist-get 'blue-warmer palette)) 1) "\" # blue-warmer\n"
-      "base0E: \"" (substring (car (alist-get 'blue-warmer palette)) 1) "\" # blue-warmer\n"
-      "base0F: \"" (substring (car (alist-get 'cyan palette)) 1) "\" # cyan\n"
+      "base0E: \"" (substring (car (alist-get 'magenta-cooler palette)) 1) "\" # magenta-cooler\n"
+      "base0F: \"" (substring (car (alist-get 'yellow palette)) 1) "\" # yellow\n"
       )
      nil
      (concat (file-name-directory (buffer-file-name)) (symbol-name theme) ".yaml"))))
